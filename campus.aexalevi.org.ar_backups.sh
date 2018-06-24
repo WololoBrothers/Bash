cc33b0868a5720437c8d1af0554c3970e6681566
hallvaror <40549265+hallvaror@users.noreply.github.com>
2018-06-24 19:49:39 -0300

    Scripts for backups
    Estos son los scripts que tenemos para los backups de Moodle en los servidores en DO.

---
+++ campus.aexalevi.org.ar_backups.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# campus.aexalevi.org.ar_backups
+
+# definir nombres para todos los backups, que se guardaran en sus respectivas carpetas
+
+now=$(date +"%d_%m_%Y")
+
+# database, moodledata, moodle
+
+backup_name_DB="/home/dbrothers/backups_all/campus.aexalevi.org.ar/DB_backups/DB_backup_$now.sql"
+backup_name_DT="/home/dbrothers/backups_all/campus.aexalevi.org.ar/DT_backups/DT_backup_$now.tar.gz"
+backup_name_MO="/home/dbrothers/backups_all/campus.aexalevi.org.ar/MO_backups/MO_backup_$now.tar.gz"
+
+# 1 - DB
+# pasando parametros para mysqldump
+
+db_host="localhost"
+db_name="moodle"
+db_user="moodle"
+db_pass="NOT_THE_PASSWORD"
+
+# crear dump DB
+
+mysqldump -u $db_user -p"NOT_THE_PASSWORD" -C -Q -e --create-options $db_name > $backup_name_DB
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/campus.aexalevi.org.ar/DB_backups/ -type f ! -name DB_backup_$now.sql ! -name DB_backups -delete
+
+echo "DB backup done."
+
+# 2 - moodledata
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/campus.aexalevi.org.ar/DT_backups/ -type f ! -name DT_backup_$now.tar.gz ! -name DT_backups -delete
+
+cd /var/www/
+
+# comprimir la carpeta moodledata
+# no se usa la ruta entera para la compresion, solo para el destino
+
+tar -zcf $backup_name_DT moodledata
+
+echo "moodledata folder backup done."
+
+# 3 - moodle
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/campus.aexalevi.org.ar/MO_backups/ -type f ! -name MO_backup_$now.tar.gz ! -name MO_backups -delete
+
+cd /var/www/
+
+# comprimir la carpeta moodle (public_html)
+# no se usa la ruta entera para la compresion, solo para el destino
+
+tar -zcf $backup_name_MO campus.aexalevi.org.ar
+
+echo "moodle folder backup done."
---
+++ lwmoodle.com.ar_backups.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# lwmoodle.com.ar_backups
+
+# definir nombres para todos los backups, que se guardaran en sus respectivas carpetas
+
+now=$(date +"%d_%m_%Y")
+
+# database, moodledata, moodle
+
+backup_name_DB="/home/dbrothers/backups_all/lwmoodle.com.ar/DB_backups/DB_backup_$now.sql"
+backup_name_DT="/home/dbrothers/backups_all/lwmoodle.com.ar/DT_backups/DT_backup_$now.tar.gz"
+backup_name_MO="/home/dbrothers/backups_all/lwmoodle.com.ar/MO_backups/MO_backup_$now.tar.gz"
+
+# 1 - DB
+# pasando parametros para mysqldump
+
+db_host="localhost"
+db_name="lw_moodle_utf8"
+db_user="lw_moodle_usr"
+db_pass="NOT_THE_PASSWORD"
+
+# crear dump DB. Password harcodeado!
+
+mysqldump -u $db_user -p"NOT_THE_PASSWORD" -C -Q -e --create-options $db_name > $backup_name_DB
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/lwmoodle.com.ar/DB_backups/ -type f ! -name DB_backup_$now.sql ! -name DB_backups -delete
+
+echo "DB backup done."
+
+# 2 - moodledata
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/lwmoodle.com.ar/DT_backups/ -type f ! -name DT_backup_$now.tar.gz ! -name DT_backups -delete
+
+cd /var/www/lwmoodle.com.ar/
+
+# comprimir la carpeta moodledata
+# no se usa la ruta entera para la compresion, solo para el destino
+
+tar -zcf $backup_name_DT moodledata
+
+echo "moodledata folder backup done."
+
+# 3 - moodle
+
+# eliminar los demas files
+
+find /home/dbrothers/backups_all/lwmoodle.com.ar/MO_backups/ -type f ! -name MO_backup_$now.tar.gz ! -name MO_backups -delete
+
+cd /var/www/lwmoodle.com.ar/
+
+# comprimir la carpeta moodle (public_html)
+# no se usa la ruta entera para la compresion, solo para el destino
+
+tar -zcf $backup_name_MO public_html
+
+echo "moodle folder backup done."
